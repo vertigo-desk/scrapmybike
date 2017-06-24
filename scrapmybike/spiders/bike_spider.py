0f15a0693905c214a66fdece4ed76058220d1188
vertigo-desk <vertigo.num@gmail.com>
2017-06-24 23:55:42 +0200

    good crawl

--- scrapmybike/spiders/bike_spider.py
+++ scrapmybike/spiders/bike_spider.py
@@ -7,6 +7,8 @@ class BikeSpider(scrapy.Spider):
 
     start_urls = ['http://www.procyclingstats.com/races.php?year=2017&circuit=13&ApplyFilter=Filter']
 
+    #start_urls = ['http://www.procyclingstats.com/race/Trofeo_Porreres,_Felanitx,_Ses_Salines,_Campos_2017']
+
     def parse(self, response):
         # follow links to races pages
         for href in response.css('a::attr(href)').re(r'race\.php.*=2'):
@@ -16,12 +18,13 @@ class BikeSpider(scrapy.Spider):
         for href in response.selector.xpath("//a[contains(text(),'Result')]/@href").extract():
             yield response.follow(href, self.parse_race)
 
-    def parse_race(self, response):
-        def extract_with_css(query):
-            return response.css(query).extract_first().strip()
+ 
 
-        yield {
-            'name': extract_with_css('h3.author-title::text'),
-            'birthdate': extract_with_css('.author-born-date::text'),
-            'bio': extract_with_css('.author-description::text'),
-        }
+    def parse_race(self, response):
+        for resultat in response.xpath('//div[contains(@class, "line str")]'):
+            yield {
+                'race' : response.xpath('//h1/text()').extract(),
+                'rank': resultat.xpath('span[contains(@class, "show")]/span[not(contains(@class, "time"))]/text()').extract_first(),
+                'rider' : resultat.xpath('span/a[@class="rider "]/@href').re_first(r'rider\/(.*)'),
+                'team' : resultat.xpath('span/a[@class="rider "]/@href').re_first(r'team\/(.*)'),
+            }
