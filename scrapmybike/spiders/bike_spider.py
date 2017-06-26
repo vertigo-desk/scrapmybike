d5b786251a0199d076cbe9f4ea01cc3c6683a484
vertigo-desk <vertigo.num@gmail.com>
2017-06-26 23:27:01 +0200

    date and finishers

--- scrapmybike/spiders/bike_spider.py
+++ scrapmybike/spiders/bike_spider.py
@@ -14,6 +14,7 @@ class BikeSpider(scrapy.Spider):
         for href in response.css('a::attr(href)').re(r'race\.php.*=2'):
             yield response.follow(href, self.parse)
 
+
         # follow pagination links
         for href in response.selector.xpath("//a[contains(text(),'Result')]/@href").extract():
             yield response.follow(href, self.parse_race)
@@ -22,8 +23,15 @@ class BikeSpider(scrapy.Spider):
 
     def parse_race(self, response):
         for resultat in response.xpath('//div[contains(@class, "line str")]'):
+            day = str(response.selector.xpath('//div[@class="subDiv info show"]/text()[1]').extract_first())
+            day = day+ " " + str(response.selector.xpath('//div[@class="subDiv info show"]/text()[2]').extract_first())
+            ranks = response.xpath('//span[contains(@class, "show")]/span[not(contains(@class, "time")) and not(contains(@style,"tahoma"))]/text()').re(r'(\d+)')
+            maxrank = max(map(int, ranks))
+            
             yield {
                 'race' : response.xpath('//h1/text()').extract(),
+                'date' : day,
+                'finishers' : maxrank,
                 'rank': resultat.xpath('span[contains(@class, "show")]/span[not(contains(@class, "time"))]/text()').extract_first(),
                 'rider' : resultat.xpath('span/a[@class="rider "]/@href').re_first(r'rider\/(.*)'),
                 'team' : resultat.xpath('span/a[@class="rider "]/@href').re_first(r'team\/(.*)'),
