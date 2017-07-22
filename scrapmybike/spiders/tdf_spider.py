6f1d1a0f240d98382cf1a93ea4e0e5eb74167469
vertigo-desk <vertigo.num@gmail.com>
2017-07-22 14:43:18 +0200

    update data

--- scrapmybike/spiders/tdf_spider.py
+++ scrapmybike/spiders/tdf_spider.py
@@ -4,6 +4,7 @@ from scrapy import Spider
 import datetime
 import csv
 import unicodedata
+import re
 
 
 class BikeSpider(Spider):
@@ -19,10 +20,10 @@ class BikeSpider(Spider):
     def __init__(self):
         with open('riders.csv', mode='r') as infile:
             reader = csv.reader(infile)
-            self.mydict = {rows[0]:[rows[1]] for rows in reader}
+            self.mydict = {rows[0]:[rows[1],0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] for rows in reader}
 
     start_urls = []
-    for i in range(1,10):
+    for i in range(1,22):
         start_urls = start_urls+[("http://www.letour.fr/le-tour/2017/fr/%d00/classement/bloc-classement-page/ITG.html" % i)]
 
     def parse(self, response):
@@ -30,17 +31,37 @@ class BikeSpider(Spider):
             return ''.join(c for c in unicodedata.normalize('NFD', s)
                 if unicodedata.category(c) != 'Mn')
 
+        # extract stage number from url
+        etape = response.url
+        m = re.search('http://www.letour.fr/le-tour/2017/fr/(.+?)00/classement/bloc-classement-page/ITG.html', etape)
+        if m:
+            no_etape = m.group(1)
+
+        # extract all results from stage
         for resultat in response.css('tbody').xpath('tr'):
             rank = resultat.xpath('td/text()')[0].extract()
             rider = strip_accents(resultat.xpath('td/a/text()')[0].extract())
             #team = resultat.xpath('td/a/text()')[1].extract()
             rez = self.mydict[rider]
-            rez.append(int(rank))
+            #rez.append(int(rank))
+            rez[int(no_etape)] =  int(rank)
             self.mydict[rider] = rez
       
 
     def spider_closed(self, spider):
         with open('tdfres.csv', 'wb') as csv_file:
             writer = csv.writer(csv_file)
-            for key, value in self.mydict.items():
+            fortuneo = ["FEILLU Brice", "PICHON Laurent", "BOUET Maxime", "PERICHON Pierre-Luc", "MC LAY DANIEL", "VACHON Florian", "HARDY Romain","GESBERT Elie", "SEPULVEDA Eduardo", "BARDET Romain","BAKELANTS Jan","DOMONT Axel","FRANK Mathias","GASTAUER Ben","GAUTIER Cyril","LATOUR Pierre-Roger","NAESEN Oliver","VUILLERMOZ Alexis"]
+            writer.writerow(["epreuve", "nbcoureurs"]+(map(lambda s: s.replace(" ", "_"), fortuneo)))
+
+            
+            for i in range(1,22):
+                result_etape = []
+                for rider in fortuneo:
+                    result_etape.append(self.mydict[rider][i])
+
+                writer.writerow([i]+[198]+result_etape)
+
+            '''for key, value in self.mydict.items():
                 writer.writerow([key.encode('utf-8')]+value)
+            '''
